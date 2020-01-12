fcb4b526e7e6cafe9e84dd08bc2d129744ac3e27
broosebrouck <benjamin.roosebrouck@etu.u-bordeaux.fr>
2020-01-13 00:03:49 +0100

    change movie index

--- src/components/Movie/Index.vue
+++ src/components/Movie/Index.vue
@@ -1,18 +1,22 @@
 <template>
     <div>
-        <h1>Film List :</h1>
-        <div class="input-group mb-3">
-            <div class="input-group-prepend">
-                <span class="input-group-text" id="inputGroup-sizing-default">Recherche</span>
+        <div class="container">
+            <h1>Film List :</h1>
+            <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                    <span class="input-group-text" id="inputGroup-sizing-default">Recherche</span>
+                </div>
+                <input type="text" class="form-control" aria-label="Sizing example input"
+                    aria-describedby="inputGroup-sizing-default" v-model="search">
             </div>
-            <input type="text" class="form-control" aria-label="Sizing example input"
-                aria-describedby="inputGroup-sizing-default" v-model="search">
+            <ul>
+                <div class="row">
+                    <MovieItem v-for="(m,i) of filteredMovie" v-bind:key="m.title" v-bind:m="m" v-bind:i="i" v-on:delete-movie="deleteMovie(i)"
+                        v-on:show-movie="showMovie(i)" v-on:edit-movie="editMovie(i)">
+                    </MovieItem>
+                </div>
+            </ul>
         </div>
-        <ul>
-            <MovieItem v-for="(m,i) of filteredMovie" v-bind:key="m.title" v-bind:m="m" v-bind:i="i" v-on:delete-movie="deleteMovie(i)"
-                v-on:show-movie="showMovie(i)" v-on:edit-movie="editMovie(i)">
-            </MovieItem>
-        </ul>
     </div>
 </template>
 
@@ -32,22 +36,16 @@ export default {
         MovieItem
     },
     methods: {
-        showMovie(i) {
-            this.$emit('show-movie', i);
-        },
-        editMovie(i) {
-            this.$emit('edit-movie', i);
-        },
         deleteMovie(i) {
-            this.$emit('delete-movie', i);
+            this.movies.splice(i, 1);
         }
     },
     computed: {
       filteredMovie() {
           return this.movies.filter(post => {
               return post.title.toLowerCase().includes(this.search.toLowerCase())
-                      || post.real.toLowerCase().includes(this.search.toLowerCase())
-                      || post.date.toString().includes(this.search)
+                      || post.dire.toLowerCase().includes(this.search.toLowerCase())
+                      || post.date.toString().includes(this.search.toLowerCase())
           })
       }
     },
--- src/components/Movie/Item.vue
+++ src/components/Movie/Item.vue
@@ -1,39 +1,22 @@
 <template>
-    <li>
-        <div v-show="!m.edit">
-            <h2>{{ m.title }}</h2> 
-            <sub>{{ m.date }}</sub> 
+    <li class="col-5 ">
+        <div class="card mb-3">
+            <h3 class="card-header">{{m.title}}</h3>
+            <img style="height: 200px; width: 100%; display: block;" :src="m.img" alt="Card image">
+            <div class="card-body">
+                <router-link :to="'/details/' + m.id" class="btn btn-primary mr-1">Details</router-link>
+                <button type="button" class="btn btn-danger" v-on:click="deleteMovie(i)">Supprimer</button> 
+            </div>
         </div>
-        <div class="front-color"></div>
-        <div v-show="m.edit"> 
-            <input class="form-control" type="text" v-model="m.title"> 
-            <input class="w-25 mt-1 form-control" type="date" v-model="m.date"> 
-            <textarea class="w-100 rounded mt-1" type="text" v-model="m.desc"></textarea> 
-        </div> 
-        <div v-if="m.show && !m.edit"> 
-            {{ m.desc }} 
-        </div> 
-        <div class="d-flex justify-content-end"> 
-            <router-link :to="'/details/' + m.id" tag="button" class="btn btn-success m-1">Détails</router-link>
-            <button type="button" class="btn btn-danger m-1" v-on:click="deleteMovie(i)">Supprimer</button>
-            <router-link :to="'/edit/' + m.id" tag="button" class="btn btn-warning m-1">Éditer</router-link>
-        </div> 
-        <hr> 
     </li>
 </template>    
 <script>
     export default {
         props: ['m', 'i'],
         methods: {
-            showMovie(i) {
-                this.$emit('show-movie', i);
-            },
-            editMovie(i) {
-                this.$emit('edit-movie', i);
-            },
             deleteMovie(i) {
                 this.$emit('delete-movie', i);
-            }
+            },
         }
     }
 </script>
